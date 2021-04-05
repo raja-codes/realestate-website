734ba9481ee1b64dc63be91e18f657bb473bb181
rajadevpy <rajathe112233@gmail.com>
2021-04-05 11:55:44 +0530

    realestate-website

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from "../images/interior-1.jpg";
+import ImageTwo from "../images/house-5.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, eos totam architecto minus facere nemo eveniet at perferendis similique incidunt?",
+  paragraphTwo:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente sint commodi hic impedit et similique quasi reprehenderit recusandae vitae adipisci?",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, eos totam architecto minus facere nemo eveniet at perferendis similique incidunt?",
+  paragraphTwo:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente sint commodi hic impedit et similique quasi reprehenderit recusandae vitae adipisci?",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
